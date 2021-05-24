740776f956b2c50dc212586a796078309bcbec0d
Gama Aristondo <gamaliel@cs.ucsb.edu>
2021-05-23 22:33:12 -0700

    Header files made

---
+++ Friendships.h
@@ -0,0 +1,23 @@
+// Gama Aristondo, 8404071
+//
+// Friendships.h
+//
+// Header file for the graph representation of friendships (adjacency list),
+// each item (user) in the list will also have pointer to the file as to where their information is
+
+#ifndef FRIENDSHIPS_H
+#define FRIENDSHIPS_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Friendships
+{
+    public:
+
+    private:
+
+};
+
+#endif
---
+++ Users.h
@@ -0,0 +1,19 @@
+// Gama Aristondo, 8404071
+//
+// Users.h
+//
+// Header file for the RBT representation of the users in the system
+// each user will have a pointer (index) to the graph and file
+
+#ifndef USERS_H
+#define USERS_H
+
+class Users
+{
+    public:
+
+    private:
+
+};
+
+#define
