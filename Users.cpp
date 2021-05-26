54ed5027f7d14a3afa6cd6b1661c31f7ca8e8ca4
Gama Aristondo <gamaliel@cs.ucsb.edu>
2021-05-25 23:33:14 -0700

    working on rbt

--- Users.cpp
+++ Users.cpp
@@ -13,14 +13,46 @@ Users::Users() {
     this->root = NULL;
 }
 
-Users::~Users() {}
+Users::~Users() {
+    clearTree(this->root);
+}
+
+void clearTree(Node *p) {
+    if(!p) {
+        return;
+    }
+    clearTree(p->left);
+    clearTree(p->right);
+    delete p;
+}
 
 void Users::add_user(string name) {
+    Node *p = new Node;
+    p->name = name;
+    p->color = 0;
+    root = insert(this->root, p);
     return;
 }
 
-string User::find_user(string name) {
-    return "";
+Node* insert(Node *root, Node *p) {
+    if(this->root == NULL) {
+        this->root = p;
+    }
+    else if(insert->name < root->name) {
+        root->left = insert(root->left, p);
+        root->left->parent = root;
+    }
+    else if(insert->name > root->name) {
+        root->right = insert(root->right, p);
+        root->right->parent = root;
+    }
+    return root;
+}
+
+int User::find_user(string name) {
+    int index;
+    // stub
+    return index;
 }
 
 vector<string> User::range_search(string name1, string name2) {
